9fec0acd7ce1f707792ab53e65382a764f409ba8
filip2600 <filip2600@gmail.com>
2017-12-18 20:50:43 +0100

    Add files via upload

--- Wyglad.java
+++ Wyglad.java
@@ -2,6 +2,9 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,7 +22,8 @@ import javafx.scene.layout.Border;
 public class Wyglad {
 	
 	static int i=0;
-	static int j=0;
+	static int h=0;
+   static int d=0;
 	
 	
 	static ArrayList<String> loginy=new ArrayList<String>();
@@ -29,7 +33,8 @@ public class Wyglad {
 	static Connection c=null;
 	static int czyznalazlo=0;
 	static int czyznalazloh=0;
-	static int konta=0;
+	static int czyznalazlo2=0;
+	static int czyznalazloh2=0;
 	public static void main(String[] args) {
 
 
@@ -54,6 +59,12 @@ public class Wyglad {
 	   ramka.add(ok,BorderLayout.EAST);
 		ramka.setSize(400,100);
 		ramka.show();
+		ramka.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent windowEvent)
+			{
+				System.exit(0);
+			}
+		});
 		
 		
 		
@@ -65,50 +76,109 @@ public class Wyglad {
 		PreparedStatement ps = null;
 		ResultSet odpowiedz = null;
 		c=p.polaczenie();
-		while(konta!=1)
-		{
+		
 			//tu bede zajmowal sie loginem
-			while(czyznalazlo!=1)
+			while(czyznalazlo!=2)
 			{
 			try
 			{
+				
+				
 				String wylosowane=probylogin();
 				String query="select * from Uzytkownicy where Nick=?";
 				ps=c.prepareStatement(query);
 				ps.setString(1,wylosowane); //tu ma szukac
 				odpowiedz=ps.executeQuery();
-				System.out.println(konta);
+				
+				
 				while(odpowiedz.next())
 				{
 					
 					czyznalazlo++;
-					i=0;
-					System.out.println(odpowiedz.getString(1));
+					czyznalazloh=0;
+					h=0;
+				 
+					
+					System.out.println("ODP: "+odpowiedz.getString(1));
 				 loginy.add(odpowiedz.getString(1));
+				 
 				 funkcjahaslo(odpowiedz.getString(1));
-				 konta++;
+				
 				 
 					
 					
 					
 				}
-				System.out.println(konta);
 				
+				ps.close();
+				odpowiedz.close();
 			}
+			
 			catch (Exception e)
 			{
 				
 			}
 		}
 		
+		i=0;
+		while(czyznalazlo2!=1)
+		{
+		try
+		{
+			String wylosowane=probylogin2();
+			String query="select * from Uzytkownicy where Nick=?";
+			ps=c.prepareStatement(query);
+			ps.setString(1,wylosowane); //tu ma szukac
+			odpowiedz=ps.executeQuery();
+	
+			while(odpowiedz.next())
+			{
+				
+				czyznalazlo2++;
+				i=0;
+			
+			 loginy.add(odpowiedz.getString(1));
+			 funkcjahaslo2(odpowiedz.getString(1));
+			 czyznalazloh=0;
+			 
+			 
+				
+				
+				
+			}
+			
+			
 		}
-		try {
-			odpowiedz.close();
-			ps.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	 
+	   
+		catch (Exception e)
+		{
+			
 		}
+	}
+	
+	
+	try {
+		odpowiedz.close();
+		ps.close();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+	for(int z=0;z<loginy.size();z++)
+	{
+		System.out.println(loginy.get(z));
+		System.out.println(hasla.get(z));
+	}
+
+	
+		
+		
+		
+		
+		
+	
 	
 	}
   
@@ -125,36 +195,48 @@ public class Wyglad {
 	static String probylogin()
 	{
 		String zwrotna = null;
+  
+		
+		
 		if(i<26)
 		{
-		zwrotna=Character.toString((char)(i+97));
-					login.setText(Character.toString((char)(i+97)));
+			zwrotna=Character.toString((char)(i+97));
+			login.setText(Character.toString((char)(i+97)));
+		
+				
+		
 					i++;
 				
 		}
+
+	
 		return zwrotna;
 
 		
 	}
 	static String probyhaslo()
 	{
-		System.out.println("Weszklo w p haslo");
+		
 		String zwrotna = null;
-		if(i<26)
+		if(h<26)
 		{
-		zwrotna=Character.toString((char)(i+97));
-					haslo.setText(Character.toString((char)(i+97)));
-					i++;
+		zwrotna=Character.toString((char)(h+97));
+					haslo.setText(Character.toString((char)(h+97)));
+					h++;
 				
 		}
+		
+		
+		//jesli i bedzie 25 na jedynkach to break
 		return zwrotna;
 	}
 	
 	static void funkcjahaslo(String nick)
 	{
-		System.out.println("Weszklo w p funkcje ");
+		
 		while(czyznalazloh!=1)
 		{
+			
 		try
 		{
 			String wylosowaneh=probyhaslo();
@@ -171,8 +253,7 @@ public class Wyglad {
 			{
 				czyznalazloh++;
 				 hasla.add(odpowiedz.getString(2));
-				
-				
+		
 				
 			}
 			
@@ -183,29 +264,88 @@ public class Wyglad {
 			
 		}
 		}
-		System.out.println(loginy.get(0));
-		System.out.println(hasla.get(0));
-	}
-	
-	
-	
-	
-	
-	
-	
 		
 	}
+	
+	
+	
+	static String probylogin2()
+	{
+		String zwrotna = null;
+		if(i<26)
+		{
+		zwrotna=Character.toString((char)(i+97))+Character.toString((char)(d+97));
+					login.setText(zwrotna);
+					i++;
+				
+		}
+		if(i==26)
+		{
+			i=0;
+			d++;
+		}
+		
+		
+	
+		
+		return zwrotna;
 
+		
+	}
+	static String probyhaslo2()
+	{
 	
-	//dopiero odlaczy od bazy jak sie uda wszystkie
-	//po skonczeniu pokazuje baze w pliku tekstowym 
-	//while konta=2 probuj
-	//gdy juz 1 znajdzie to nie powtarzaj w petli .
-	// narazie do 3 znakow tylko tylko literki a-z male
-//1.umiesc w petlach 
-//2. wymysl algorytm
+		String zwrotna = null;
+		if(i<26)
+		{
+		zwrotna=Character.toString((char)(i+97))+Character.toString((char)(d+97));
+					haslo.setText(zwrotna);
+					i++;
+				
+		}
+		if(i==26)
+		{
+			i=0;
+			d++;
+		}
+	
+		
+		//jesli i bedzie 25 na jedynkach to break
+		return zwrotna;
+	}
+	
+	static void funkcjahaslo2(String nick)
+	{
+	
+		while(czyznalazloh2!=1)
+		{
+		try
+		{
+			String wylosowaneh=probyhaslo2();
+			
+			String query="select * from Uzytkownicy where Haslo=? and Nick=?";
+			PreparedStatement ps=c.prepareStatement(query);
+			ps.setString(1,wylosowaneh);
+			ps.setString(2,nick);
+			ResultSet odpowiedz=ps.executeQuery();
+			while(odpowiedz.next())
+			{
+				czyznalazloh2++;
+				 hasla.add(odpowiedz.getString(2));
+		
+				
+			}
+			
+			
+		}
+		catch (Exception e)
+		{
+			
+		}
+		}
+		
+	}
+	
 
-	
-	
-	
-
+		
+	}
